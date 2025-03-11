399475f8c2bf68705e38cff53aae158e2ec4ddd9
moises.santos <moises.efsantos@gmail.com>
2025-03-11 16:57:29 -0300

    update researcher panel

--- pages/researcher.py
+++ pages/researcher.py
@@ -4,7 +4,6 @@ import streamlit as st
 import psycopg2
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'services')))
-
 from database_connection import create_connection
 
 def get_stores():
@@ -15,17 +14,30 @@ def get_stores():
     conn.close()
     return stores
 
-def get_vehicles():
+def get_brands():
     conn = create_connection()
     cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM brand;")
+    brands = cursor.fetchall()
+    conn.close()
+    return brands
 
+def get_models_by_brand(brand_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM model WHERE brand_id = %s;", (brand_id,))
+    models = cursor.fetchall()
+    conn.close()
+    return models
+
+def get_vehicles_by_model(model_id):
+    conn = create_connection()
+    cursor = conn.cursor()
     cursor.execute("""
-        SELECT v.id, b.name AS brand, m.name AS model, v.fabrication_year, v.model_year, v.average_price
-        FROM vehicles v
-        JOIN model m ON v.model_id = m.id
-        JOIN brand b ON m.brand_id = b.id;
-    """)
-
+        SELECT id, fabrication_year, model_year, average_price
+        FROM vehicles
+        WHERE model_id = %s;
+    """, (model_id,))
     vehicles = cursor.fetchall()
     conn.close()
     return vehicles
@@ -42,27 +54,48 @@ def save_price(store_id, vehicle_id, price, collect_date):
 
 def researcher_panel():
     st.title("Painel do Pesquisador")
-    
+
     stores = get_stores()
-    store_options = [store[1] for store in stores]
-    store_selected = st.selectbox("Selecione a loja", store_options)
-    store_id = stores[store_options.index(store_selected)][0]
-    
-    vehicles = get_vehicles()
-    vehicle_options = [f"{vehicle[1]} {vehicle[2]} ({vehicle[3]} - {vehicle[4]})" for vehicle in vehicles]
-    vehicle_selected = st.selectbox("Selecione o veículo", vehicle_options)
-    vehicle_id = vehicles[vehicle_options.index(vehicle_selected)][0]
+    store_options = {"Selecione...": None}
+    store_options.update({store[1]: store[0] for store in stores})
+    store_selected = st.selectbox("Selecione a loja", list(store_options.keys()))
 
-    price = st.number_input("Digite o preço", min_value=0.0, format="%.2f")
+    if store_selected != "Selecione...":
+        store_id = store_options[store_selected]
 
-    collect_date = st.date_input("Data de coleta")
+        brands = get_brands()
+        brand_options = {"Selecione...": None}
+        brand_options.update({brand[1]: brand[0] for brand in brands})
+        brand_selected = st.selectbox("Selecione a marca", list(brand_options.keys()))
 
-    if st.button("OK"):
-        if price > 0:
-            save_price(store_id, vehicle_id, price, collect_date)
-            st.success("Preço registrado com sucesso!")
-        else:
-            st.error("Por favor, insira um preço válido.")
+        if brand_selected != "Selecione...":
+            brand_id = brand_options[brand_selected]
+
+            models = get_models_by_brand(brand_id)
+            model_options = {"Selecione...": None}
+            model_options.update({model[1]: model[0] for model in models})
+            model_selected = st.selectbox("Selecione o modelo", list(model_options.keys()))
+
+            if model_selected != "Selecione...":
+                model_id = model_options[model_selected]
+
+                vehicles = get_vehicles_by_model(model_id)
+                vehicle_options = {"Selecione...": None}
+                vehicle_options.update({f"{v[2]}": v[0] for v in vehicles})
+                vehicle_selected = st.selectbox("Selecione o ano do modelo do veículo", list(vehicle_options.keys()))
+
+                if vehicle_selected != "Selecione...":
+                    vehicle_id = vehicle_options[vehicle_selected]
+
+                    price = st.number_input("Digite o preço", min_value=0.0, format="%.2f")
+                    collect_date = st.date_input("Data de coleta")
+
+                    if st.button("OK"):
+                        if 0 < price < 100000000.00:
+                            save_price(store_id, vehicle_id, price, collect_date)
+                            st.success("Preço registrado com sucesso!")
+                        else:
+                            st.error("Por favor, insira um preço válido.")
 
 if __name__ == "__main__":
     researcher_panel()
