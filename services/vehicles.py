5251f3e3346f0c23b9af644d5c352b51fd072a90
moises.santos <moises.efsantos@gmail.com>
2025-03-11 16:24:31 -0300

    update vehicles.py

--- services/vehicles.py
+++ services/vehicles.py
@@ -1,32 +1,22 @@
-import psycopg2
-import streamlit as st
-from database_connection import init_connection
+from services.database_connection import create_connection, table_exists
 
 def create_vehicles_table():
-    conn = init_connection()
-    if conn is None:
-        return
-
-    try:
+    if not table_exists("vehicles"):
+        conn = create_connection()
         cur = conn.cursor()
-        
+       
         cur.execute("""
             CREATE TABLE IF NOT EXISTS vehicles (
                 id SERIAL PRIMARY KEY,
-                model_id INTEGER REFERENCES models(id) ON DELETE CASCADE,
+                model_id INTEGER REFERENCES model(id) ON DELETE CASCADE,
                 fabrication_year INTEGER NOT NULL,
                 model_year INTEGER NOT NULL,
                 average_price DECIMAL(10,2)
             );
         """)
-        
         conn.commit()
-        
-        st.success("Tabela 'vehicles' criada com sucesso!")
-        
-    except psycopg2.Error as e:
-        st.error(f"Erro ao criar a tabela 'vehicles': {e}")
-    
-    finally:
         cur.close()
         conn.close()
+        print("Tabela 'vehicles' criada com sucesso.")
+    else: 
+        print("Tabela 'vehicles' já existe.")         
