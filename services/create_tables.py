7fee7d7b1891c6cc4b1ef1b7c1be286741472a7d
hugopires17 <hugopprocopio@hotmail.com>
2025-03-18 16:43:07 -0300

    monthly_dolar tabel creation and CRUD

--- services/create_tables.py
+++ services/create_tables.py
@@ -6,6 +6,7 @@ from services.vehicles import create_vehicles_table
 from services.store import create_store_table
 from services.prices import create_prices_table
 from services.price_changes import create_price_changes_table
+from services.monthly_dolar import create_monthly_dolar_table
 
 def create_all_tables():
     create_connection()
@@ -17,6 +18,7 @@ def create_all_tables():
         create_store_table()
         create_prices_table()
         create_price_changes_table()
+        create_monthly_dolar_table
         print("Todas as tabelas foram criadas com sucesso.")
     except Exception as e:
         print(f"Erro ao criar tabelas: {e}")
